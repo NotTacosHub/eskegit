eedd05c3dac466dc18f5723d0fa81b1e0bf47e89
NotTacosHub <smithclayton80@gmail.com>
2021-10-13 20:46:24 -0500

    initial commit: part 2

---
+++ src/bootstrap.js
@@ -0,0 +1,7 @@
+import { multiply } from './helper';
+
+console.log(multiply(2, 5));
+
+import navigation, { greeting, multiply } from './navigation';
+
+console.log(navigation());
---
+++ src/navigation.js
@@ -0,0 +1,10 @@
+export default function () {
+
+}
+
+
+export function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+export const greeting = 'Hi there';
